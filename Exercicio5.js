c35054cf124694c8f2a8bce753055f2ecd16394c
devbrayner <dev.brayner@outlook.com>
2024-09-20 22:36:36 -0300

    até o exercicio 5

--- Exercicio5.js
+++ Exercicio5.js
@@ -1,4 +1,7 @@
-const Nota1 = 10
-const Nota2 = 4
-const Nota3 = 6
-console.log(`O resultado da média é ${((Nota1 + Nota2 + Nota3) / 3).toFixed(2)}`)
+const prompt = require('prompt-sync')();
+
+var Nota1 = parseFloat(prompt("digite a primeira nota: "))
+var Nota2 = parseFloat(prompt("digite a segunda nota: "))
+var Nota3 = parseFloat(prompt("digite a terceira nota: "))
+var media = ((Nota1 + Nota2 + Nota3) / 3).toFixed(2)
+console.log(`O resultado da média é ${media}`)
